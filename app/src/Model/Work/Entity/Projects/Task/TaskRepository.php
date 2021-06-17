6d7f72ae6e9ded11a5fc9927175de1754541e964
sergalas <hudosg@gmail.com>
2021-06-17 10:14:50 +0300

    add usecases

--- app/src/Model/Work/Entity/Projects/Task/TaskRepository.php
+++ app/src/Model/Work/Entity/Projects/Task/TaskRepository.php
@@ -4,16 +4,28 @@ declare(strict_types=1);
 namespace App\Model\Work\Entity\Projects\Task;
 
 use Doctrine\ORM\EntityManagerInterface;
+use App\Exceptions\EntityNotFoundException;
 
 class TaskRepository
 {
     private $em;
     private $connection;
+    private $repo;
 
     public function __construct(EntityManagerInterface $em)
     {
         $this->em = $em;
+        $this->repo=$em->getRepository(Task::class);
         $this->connection = $em->getConnection();
+    }
+
+    public function get(Id $id): Task
+    {
+        /** @var Task $task */
+        if (!$task = $this->repo->find($id->getValue())) {
+            throw new EntityNotFoundException('Task is not found.');
+        }
+        return $task;
     }
 
     public function add(Task $task): void
@@ -21,8 +33,13 @@ class TaskRepository
         $this->em->persist($task);
     }
 
+    public function remove(Task $task): void
+    {
+        $this->em->remove($task);
+    }
+
     public function nextId(): Id
     {
-        return new Id((int)$this->connection->query('SELECT nextval(\'work_projects_tasks_seq\')')->fetchColumn());
+        return new Id((int)$this->connection->executeQuery('SELECT nextval(\'work_projects_tasks_seq\')'));
     }
 }
---
+++ app/src/ReadModel/Work/Projects/Task/TaskFetcher.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace App\ReadModel\Work\Projects\Task;
+
+use App\ReadModel\Fetcher;
+
+class TaskFetcher extends Fetcher
+{
+
+}
